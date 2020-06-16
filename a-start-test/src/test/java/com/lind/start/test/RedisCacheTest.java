a36bd892a9720d2b7028f29ce1c233ca96b96edb
Lind <bfyxzls@sina.com>
2020-06-16 16:18:13 +0800

    å添加了时间选择器

--- a-start-test/src/test/java/com/lind/start/test/RedisCacheTest.java
+++ a-start-test/src/test/java/com/lind/start/test/RedisCacheTest.java
@@ -9,6 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest()
 public class RedisCacheTest {
@@ -20,6 +25,15 @@ public class RedisCacheTest {
         redisService.set("hello-test-redis.cache", "lind");
         Assert.assertEquals("lind", redisService.get("hello-test-redis.cache"));
     }
+
+    @Test
+    public void DateValue() throws ParseException {
+        System.out.println(new Date(2020, 0, 1));//注意年和月的问题
+        System.out.println(LocalDate.of(2020, 1, 1));//推荐使用
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sdf.parse("2020-6-01"));//推荐使用
+    }
+
 
     @Test
     public void objectValue() {
