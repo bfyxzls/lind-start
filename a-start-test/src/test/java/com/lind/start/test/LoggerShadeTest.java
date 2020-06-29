15e445a7a117377c2b7acd07f6bdc93f44d239f1
Lind <bfyxzls@sina.com>
2020-06-29 17:45:45 +0800

    add shaded

---
+++ a-start-test/src/test/java/com/lind/start/test/LoggerShadeTest.java
@@ -0,0 +1,14 @@
+package com.lind.start.test;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+
+/**
+ * shade插件测试.
+ */
+public class LoggerShadeTest {
+    @Test
+    public void hello(){
+
+    }
+}
