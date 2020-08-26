9bd6dae598872308b89e4fe74411444b603882f6
Lind <bfyxzls@sina.com>
2020-08-26 17:37:20 +0800

    æobjectid转为æ有意义的信息

--- lind-common/src/test/java/com/lind/common/IdMakerTest.java
+++ lind-common/src/test/java/com/lind/common/IdMakerTest.java
@@ -4,10 +4,29 @@ import com.lind.common.util.IdMakerUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
-import java.util.concurrent.TimeUnit;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Slf4j
 public class IdMakerTest {
+    /**
+     * 时间戳转换成日期格式字符串
+     *
+     * @param seconds   精确到秒的字符串
+     * @param formatStr
+     * @return
+     */
+    public static String timeStamp2Date(Integer seconds, String format) {
+        if (seconds == null) {
+            return "";
+        }
+        if (format == null || format.isEmpty()) {
+            format = "yyyy-MM-dd HH:mm:ss";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(Long.valueOf(seconds + "000")));
+    }
+
     @Test
     public void mongodbId() throws InterruptedException {
 
@@ -17,4 +36,23 @@ public class IdMakerTest {
 
     }
 
+    @Test
+    public void hexDecConvert() {
+        String hex = "ff";
+        Integer dec = Integer.parseInt(hex, 16);
+        log.info("dec={}", dec);
+
+        String val = "5f46260501000001fc";
+
+        Integer timer = Integer.parseInt(val.substring(0, 8), 16);
+        String date = timeStamp2Date(timer, "yyyy-MM-dd HH:mm:ss");
+        log.info("time:{}", date);
+
+        Integer service = Integer.parseInt(val.substring(8, 10), 16);
+        log.info("serviceId:{}", service);
+
+        Integer inc = Integer.parseInt(val.substring(10), 16);
+        log.info("inc:{}", inc);
+
+    }
 }
