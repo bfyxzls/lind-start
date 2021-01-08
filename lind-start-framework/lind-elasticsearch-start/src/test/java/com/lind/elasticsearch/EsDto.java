85fe38b53c53f0aca41331fa00876db070dea8ce
zhangzhanling <bfyxzls@sina.com>
2021-01-08 10:17:35 +0800

    添加了es的说明文件

--- lind-start-framework/lind-elasticsearch-start/src/test/java/com/lind/elasticsearch/EsDto.java
+++ lind-start-framework/lind-elasticsearch-start/src/test/java/com/lind/elasticsearch/EsDto.java
@@ -11,22 +11,42 @@ import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.io.Serializable;
 
-@Document(indexName = "esdto", type = "esdto")
 @Data
+@Document(indexName = "esdto", shards = 1, replicas = 0)
 @ToString(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
+//@Mapping(mappingPath = "mapping/es-esdto.json")
 public class EsDto extends EsBaseEntity implements Serializable {
 
     /**
      * keyword类型不自动分词；text类型会自动分词.
      * analyzer表示开启分词功能.
+     *
+     * @filed里的analyzer是不行的，只能通过@Mapping实现,而且它会复盖实体的@field注解
      */
-    @Field(type = FieldType.Keyword, analyzer = "ik_max_word")
-    private String name;
-    private Integer age;
     @Field(type = FieldType.Keyword)
-    private String sex;
-    @Field(type = FieldType.Keyword, analyzer = "ik_max_word")
+    private String name;
+    @Field(type = FieldType.Integer)
+    private Integer age;
+    @Field(type = FieldType.Integer)
+    private Integer sex;
+    @Field(type = FieldType.Text, analyzer = "ik_max_word")
     private String desc;
+    @Field(type = FieldType.Text, analyzer = "ik_max_word")
+    private String memo;
+    @Field(type = FieldType.Nested, fielddata = true)
+    private Address address;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class Address {
+        @Field(type = FieldType.Keyword)
+        private String province;
+        @Field(type = FieldType.Keyword)
+        private String city;
+        @Field(type = FieldType.Keyword)
+        private String district;
+    }
 }
--- lind-start-framework/lind-elasticsearch-start/src/test/java/com/lind/elasticsearch/EsRepo.java
+++ lind-start-framework/lind-elasticsearch-start/src/test/java/com/lind/elasticsearch/EsRepo.java
@@ -1,6 +1,8 @@
 package com.lind.elasticsearch;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface EsRepo extends ElasticsearchRepository<EsDto, String> {
 }
