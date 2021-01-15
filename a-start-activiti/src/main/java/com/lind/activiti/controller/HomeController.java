7b136adde3e511bc5afcb548a575496f341f9c32
zhangzhanling <bfyxzls@sina.com>
2021-01-15 10:00:26 +0800

    解决工作流部署时多个swagger配置问题

--- a-start-activiti/src/main/java/com/lind/activiti/controller/HomeController.java
+++ a-start-activiti/src/main/java/com/lind/activiti/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.lind.activiti.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class HomeController {
-    @GetMapping("index")
-    public ResponseEntity index() {
-        return ResponseEntity.ok("ok");
+    @GetMapping("/")
+    public void index(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/view/model/list");
     }
 }
