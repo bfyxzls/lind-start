cbc677781ae79b12e3fcad5326388920e617832e
zhangzhanling <bfyxzls@sina.com>
2021-03-03 09:12:46 +0800

    添加了gateway的cors功能

--- a-start-micro-service/a-start-gateway/src/main/java/com/lind/GatewayApplication.java
+++ a-start-micro-service/a-start-gateway/src/main/java/com/lind/GatewayApplication.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class GatewayApplication {
-    @Value("${auth}")
+    @Value("${auth:lind}")
     private String auth;
 
-    @Value("${email}")
+    @Value("${email:lind@sina.com}")
     private String email;
 
     public static void main(String[] args) {
