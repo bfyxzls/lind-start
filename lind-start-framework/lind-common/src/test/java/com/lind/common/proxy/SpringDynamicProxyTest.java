cae1eba451bbb62b99d9ddb9d0b5b626d268b728
bfyxzls <bfyxzls@sina.com>
2021-05-15 22:22:09 +0800

    添加了spring bean接口代理

---
+++ lind-start-framework/lind-common/src/test/java/com/lind/common/proxy/SpringDynamicProxyTest.java
@@ -0,0 +1,38 @@
+package com.lind.common.proxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * spring动态代理bean,用于Spring动态注入自定义接口到ioc里.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpringDynamicProxyTest {
+    @Autowired
+    CalculateService calculateService;
+    @Autowired
+    TestService testService;
+
+    @Test
+    public void testProxy() {
+        calculateService.getResult("lind");
+        calculateService.insert("ok");
+
+    }
+
+    public interface CalculateService {
+
+        String getResult(String name);
+
+        void insert(String entity);
+    }
+
+    public interface TestService {
+
+        String getList(String code, String name);
+    }
+}
