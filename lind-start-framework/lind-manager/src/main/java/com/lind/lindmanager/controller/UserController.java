3ce39840ed58bd7cb243a1d848c32e0a72447939
bfyxzls <bfyxzls@sina.com>
2021-06-27 19:42:31 +0800

    add vue demo

---
+++ lind-start-framework/lind-manager/src/main/java/com/lind/lindmanager/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.lind.lindmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("users")
+public class UserController {
+    @GetMapping("hello")
+    public String hello(){
+        return "user/list";
+    }
+}
