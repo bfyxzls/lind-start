ff998ec17b0ec8d13aa4b7ac4910f353eecf138e
bfyxzls <bfyxzls@sina.com>
2021-06-28 20:51:16 +0800

    添加检索

--- lind-start-framework/lind-manager/src/main/java/com/lind/lindmanager/controller/MessageController.java
+++ lind-start-framework/lind-manager/src/main/java/com/lind/lindmanager/controller/MessageController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("user")
-public class UserController {
+@RequestMapping("message")
+public class MessageController {
     @GetMapping("list")
     public String hello(){
-        return "user/list";
+        return "message/list";
     }
 }
