30cce38c27800e353f9acb6e4d8055e3b5292501
zhangzhanling <bfyxzls@sina.com>
2021-07-30 17:47:28 +0800

    redis包添加缓存策略

--- lind-start-framework/lind-redis-start/src/main/java/com/lind/redis/configure/RedisCacheConfiguration.java
+++ lind-start-framework/lind-redis-start/src/main/java/com/lind/redis/configure/RedisCacheConfiguration.java
@@ -1,8 +1,7 @@
-package com.lind.lindmanager.controller;
+package com.lind.redis.configure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -12,11 +11,11 @@ import java.time.Duration;
  * 自定义序列化为json，避免使用二进制，每个类都需要实现Serializable接口.
  */
 @Configuration
-public class CustomerRedisCacheConfiguration {
+public class RedisCacheConfiguration {
     @Bean
-    public RedisCacheConfiguration redisCacheConfiguration() {
+    public org.springframework.data.redis.cache.RedisCacheConfiguration redisCacheConfiguration() {
         Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
+        org.springframework.data.redis.cache.RedisCacheConfiguration configuration = org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig();
         configuration = configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
                 .entryTtl(Duration.ofDays(30));
         return configuration;
