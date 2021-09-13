2383078d71ba6921c7168d0b36e72af8d76e26ff
zhangzhanling <bfyxzls@sina.com>
2021-09-13 20:03:05 +0800

    add retry methods

--- lind-start-framework/lind-common/src/test/java/com/lind/common/MinibaseTest.java
+++ lind-start-framework/lind-common/src/test/java/com/lind/common/MinibaseTest.java
@@ -1,15 +1,27 @@
 package com.lind.common;
 
-import com.lind.common.minibase.*;
+import com.lind.common.minibase.Bytes;
+import com.lind.common.minibase.Config;
+import com.lind.common.minibase.KeyValue;
+import com.lind.common.minibase.MStore;
+import com.lind.common.minibase.MiniBase;
 import lombok.SneakyThrows;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
 
-import java.io.File;
-import java.io.RandomAccessFile;
-
-import static com.lind.common.minibase.DiskFile.TRAILER_SIZE;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MinibaseTest {
+    Logger log = LoggerFactory.getLogger(MinibaseTest.class);
+    private volatile ConcurrentSkipListMap<Integer, String> kvMap;
+    private volatile ConcurrentHashMap<Integer, String> kvHashMap;
+
     @SneakyThrows
     @Test
     public void write() {
@@ -31,16 +43,57 @@ public class MinibaseTest {
 
     @SneakyThrows
     @Test
-    public void read() {
-        File f = new File("d:\\miniBase\\data.00000000000000000000.tmp");
-        RandomAccessFile randomAccessFile = new RandomAccessFile(f, "r");
+    public void multiple() {
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        for (int i = 1; i < 40; i++) {
+            pool.submit(new PrintTask(i));
+        }
+        pool.shutdown();//已经添加到线程池的将继续执行，执行完成后shutdown.
+        //assert kvMap.size() == 40;
+        while (Thread.activeCount() > 1) { //判断活动线程数量
+            Thread.yield(); //线程把CPU时间让掉，让其他或者自己的线程执行，就是不让主线程结束，等待没有活动线程了，再结束
+        }
 
-        long fileSize = f.length();
-        assert fileSize > TRAILER_SIZE;
-        randomAccessFile.seek(fileSize - TRAILER_SIZE);
+    }
 
-        byte[] buffer = new byte[8];
-        assert randomAccessFile.read(buffer) == buffer.length;
-        assert fileSize == Bytes.toLong(buffer);
+    @Test
+    public void skipListTest() {
+
+        for (int i = 1; i < 10000; i++)
+            kvHashMap.put(i, "a");
+        for (int i = 1; i < 10000; i++)
+            kvMap.put(i, "a");
+        StopWatch stopwatch = new StopWatch();
+        stopwatch.start(" hashmap find");
+        kvMap.containsKey(4);
+        stopwatch.stop();
+        stopwatch.start(" skipList find");
+        kvHashMap.contains(4);
+        stopwatch.stop();
+
+        log.info("stopwatch result {}", stopwatch.prettyPrint());
+
+    }
+
+    @Before
+    public void init() {
+        kvMap = new ConcurrentSkipListMap<>();
+        kvHashMap = new ConcurrentHashMap<>();
+    }
+
+    private class PrintTask implements Runnable {
+        private Integer i;
+
+        public PrintTask(Integer i) {
+            this.i = i;
+        }
+
+        @SneakyThrows
+        @Override
+        public void run() {
+            kvMap.put(i, i.toString());
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
     }
 }
