32fb55ca192275864d66c8954943ad3857548d1c
zhangzhanling <bfyxzls@sina.com>
2021-09-17 14:07:19 +0800

    add @valid test

--- lind-start-framework/lind-common/src/test/java/com/lind/common/collection/CollectionTest.java
+++ lind-start-framework/lind-common/src/test/java/com/lind/common/collection/CollectionTest.java
@@ -10,12 +10,41 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 集合操作
  */
 public class CollectionTest {
     List<Student> students = new ArrayList<Student>();
+
+    @Test
+    public void test1(){
+        List<Integer> list1=new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+
+        List<Integer> list2=new ArrayList<>();
+        list2.add(3);
+        list2.add(4);
+        list2.add(5);
+
+        System.out.println("====求交集===");
+        List<Integer> list=list1.stream().filter(t->list2.contains(t)).collect(Collectors.toList());
+        list.stream().forEach(System.out::println);
+
+        System.out.println("====求差集===");
+        list=list1.stream().filter(t-> !list2.contains(t)).collect(Collectors.toList());
+        list.stream().forEach(System.out::println);
+
+
+        System.out.println("====求并集===");
+        list.addAll(list1);
+        list.addAll(list2);
+        list=list.stream().distinct().collect(Collectors.toList());
+        list.stream().forEach(System.out::println);
+    }
 
     @Test
     public void filterField() {
