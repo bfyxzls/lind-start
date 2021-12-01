e9f952e10fc1138a57e20ad41379c2b8f75685e2
Lind <bfyxzls@sina.com>
2021-12-01 20:53:50 +0800

    添加了一致性hash说明

---
+++ lind-start-framework/lind-common/src/test/java/com/lind/common/util/ConsistenceHashUtilTest.java
@@ -0,0 +1,10 @@
+package com.lind.common.util;
+
+import org.junit.Test;
+
+public class ConsistenceHashUtilTest {
+    @Test
+    public void test() {
+        new ConsistenceHashUtil().test();
+    }
+}
