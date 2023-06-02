2ff7b6a299ddb28a76e3eb3045b5535a3f4fb3a1
lindzhang <bfyxzls@sina.com>
2023-06-02 10:29:14 +0800

    添加了redisson锁

--- lind-test-lock/src/main/java/com/lind/lock/AppApplication.java
+++ lind-test-lock/src/main/java/com/lind/lock/AppApplication.java
@@ -1,8 +1,8 @@
 package com.lind.lock;
 
-import com.lind.redis.lock.template.Callback;
-import com.lind.redis.lock.template.DistributedLockTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,39 +23,47 @@ import java.util.concurrent.TimeUnit;
 public class AppApplication {
 
 	@Autowired
-	DistributedLockTemplate distributedLockTemplate;
+	RedissonClient redissonClient;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
-	void lock5Second() {
-		// 锁最大排除时等待的时间，超过这个时间还没拿到锁，那就放弃.
-
-		distributedLockTemplate.execute("订单流水号", 70, TimeUnit.SECONDS, new Callback() {
-			@Override
-			public Object onGetLock() throws InterruptedException {
-				// 获得锁后要做的事
-				log.info("{} 拿到锁，需要5秒钟，这时有请求打入应该被阻塞或者拒绝", Thread.currentThread().getName());
-				TimeUnit.SECONDS.sleep(80);
-				log.info("大任务执行完成");
-				return null;
+	String lock5Second(String orderId) {
+		//初始化锁的对象
+		RLock rLock = redissonClient.getLock("lock_" + orderId);
+		try {
+			//尝试加锁, 最多等待5秒
+			boolean lock = rLock.tryLock(20, -1, TimeUnit.SECONDS);
+			if (lock) {
+				log.info("获取到锁，执行支付流程");
+				//延时15s
+				Thread.sleep(15000);
+				log.info("支付完成");
+				return "支付完成";
+			} else {
+				log.info("请稍等，有人正在支付");
+				return "请稍等，有人正在支付";
 			}
-
-			@Override
-			public Object onTimeout() throws InterruptedException {
-				// 获取锁超时后要做的事
-				log.info("{} 没拿到锁", Thread.currentThread().getName());
-				return null;
+		} catch (InterruptedException e) {
+			log.error("获取锁异常 e:{}", e.getMessage());
+			return "获取锁异常";
+		} finally {
+			//是锁定状态，并且是当前执行线程的锁，释放锁
+			if (rLock.isLocked() && rLock.isHeldByCurrentThread()) {
+				rLock.unlock();
 			}
-		});
+		}
 	}
 
 	@GetMapping("lock")
 	public ResponseEntity lock() {
-		Thread thread1 = new Thread(() -> lock5Second());
-		Thread thread2 = new Thread(() -> lock5Second());
-		Thread thread3 = new Thread(() -> lock5Second());
+		// 三个人同时支付同一个订单，当第一个执行15秒后，
+		// 由于第二人最多等待20秒，所以也可以拿到锁，
+		// 而第三个人没有抢到锁，它在第二个人执行一段时间后，达到了20S（第一个人用了15S，第二个人用了5S）后，他就不去等待了
+		Thread thread1 = new Thread(() -> lock5Second("o1"));
+		Thread thread2 = new Thread(() -> lock5Second("o1"));
+		Thread thread3 = new Thread(() -> lock5Second("o1"));
 
 		thread1.start();
 		thread2.start();
